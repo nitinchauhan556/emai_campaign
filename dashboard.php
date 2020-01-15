ff567cd54b84aac49ed006e910c5258d17d198ee
Nitin Chauhan <43624052+nitinchauhan556@users.noreply.github.com>
2020-01-15 11:13:17 +0530

    Update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -1,1 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.css">
+</head>
+<body>
+    <div class="container">
+      <div class="col-sm-10" style="width:600px; margin-left:200px; margin-top:50px;">
+        <div class="jumbotron">
+          <div class="form-group" style ="margin-top:-50px;">
+            <h1 style = "margin-left:150px;"> Dashboard </h1>
+          </div>
+          
 
+            <div class="form-group">
+              <button class="btn btn-primary" style="width:455px;">All Subscribers</button>
+            </div>
+
+            <div class="form-group">
+              <button class="btn btn-primary" style="width:455px;">Previous Email Campaigns</button>
+            </div>
+
+            <div class="form-group">
+              <button class="btn btn-primary" style="width:455px;">Launch Email Campaign</button>
+            </div>
+
+            <div class="form-group">
+              <button class="btn btn-primary" style="width:455px;">Add New Subscriber</button>
+            </div>
+
+            <div class="form-group">
+              <button class="btn btn-primary" style="width:455px;">Launch Campaigns For Specific Subscribers</button>
+            </div>
+
+        </div>
+      </div>
+    </div>
+    <script src="bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
+</body>
+</html>
